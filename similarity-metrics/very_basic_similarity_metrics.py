e71a666791749a523033126c1d56e8523f359f93
Nick van der Merwe <nick.jvdmerwe@gmail.com>
2021-10-04 10:23:10 +1000

    Started extending the similarity metrics to generate users too
    Took 31 minutes

--- similarity-metrics/very_basic_similarity_metrics.py
+++ similarity-metrics/very_basic_similarity_metrics.py
@@ -43,6 +43,58 @@ def embed_vectors(df, column_to_embed):
     return pd.DataFrame(series)
 
 
+def average_user(
+        embedded_movies_df: pd.DataFrame,
+        set_of_movies_to_average: int,
+        movies_id_lookup: dict
+):
+    """
+    Averages a given user
+    :param embedded_movies_df:
+    :param set_of_movies_to_average:
+    :param movies_id_lookup:
+    :return:
+    """
+    # TODO
+    # Get all of the movies the user's seen
+    pass
+
+
+def average_all_user_movies(
+        ratings_df: pd.DataFrame,
+        embedded_movies_df: pd.DataFrame,
+        movies_id_lookup: dict
+):
+    """
+    Generates a dataframe of features that describes a user's average movie
+    :param ratings_df:
+    :param embedded_movies_df:
+    :param movies_id_lookup:
+    :return:
+    """
+    # defragmenting it is going to make the next section a little faster
+    ratings_df = ratings_df.groupby(['userId'])
+    users = ratings_df['userId'].unique()
+
+    embedded_users = pd.DataFrame(
+        columns=embedded_movies_df.columns
+    )
+
+    user_lookup = {}
+
+    for user in users:
+        watched_movies = ratings_df.query('userId ==', user)['imdbId'].unique()
+
+        embedded_users.append(
+            average_user(embedded_movies_df,
+                         set_of_movies_to_average=watched_movies,
+                         movies_id_lookup=movies_id_lookup
+                         )
+        )
+
+    return embedded_users, user_lookup
+
+
 def append_columns(df1: pd.DataFrame, df2: pd.DataFrame,
                    title1='_a', title2='_g'):
     # Rename all of the columns in the first dataframe accordingly
@@ -61,16 +113,27 @@ if __name__ == "__main__":
     movie_df, rating_df = get_truncated_data()
     genre_features: pd.DataFrame = embed_vectors(movie_df, 'genre')
     actor_features: pd.DataFrame = embed_vectors(movie_df, 'actors')
-    features = append_columns(
+    movies_features = append_columns(
         genre_features, actor_features
     )
-    print(features.head())
-    features = cosine_similarity(features)
-    print("similarity")
-    print(features)
-    pickle_manager.save_lzma_pickle(
-        features, '../pickles/similarity_matrix.pickle.lz4'
+    movies_index_lookup = {}
+    # Generate a lookup of index : movieId
+    for index, row in movie_df.iterrows():
+        movies_index_lookup[row['imdbId']] = index
+
+    ratings_features = average_all_user_movies(
+        rating_df,
+        movies_features,
+        movies_index_lookup
     )
+
+    print(movies_features.head())
+    movies_similarity_matrix = cosine_similarity(movies_features)
+
+    pickle_manager.save_lzma_pickle(
+        movies_similarity_matrix, '../pickles/similarity_matrix.pickle.lz4'
+    )
+
     pickle_manager.save_lzma_pickle(
         movie_df, '../pickles/simil_movies.pickle.lz4'
     )
