4ebbcc5fdd2ae85d3c95aa00a3631e2d6d8ddf99
EthanLewis15 <15ethanlewis@gmail.com>
2021-10-10 20:38:38 +1000

    Delete ResultAnalysis.py

--- FP-Growth/ResultAnalysis.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-df_library = pd.read_csv("Library.csv")
-df_implementation = pd.read_csv("Implementation.csv")
-
-ax = df_library.plot()
-df_implementation.plot(ax=ax)
