fdd0abe765c0425917fa0c7a053a584a7e85e951
Nick van der Merwe <nick.jvdmerwe@gmail.com>
2021-10-10 16:21:26 +1000

    Fixed notebook, started R
    Took 17 minutes

---
+++ density/turn_similarity_into_dissimilarity.py
@@ -0,0 +1,12 @@
+"""
+This was previously implemented for the notebook, its just here as a standalone
+module for convenience
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
